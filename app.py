7939770c373e44f82b2d03a7773ce5fcc7c1eb06
Baibhav Kumar <147192139+baibhav16@users.noreply.github.com>
2025-04-11 12:05:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def start():
     age = data.get("age")
     gender = data.get("gender")
     session_id = str(uuid.uuid4())
-    user_sessions[session_id] = {
+    sessions[session_id] = {
         "symptoms": symptoms,
         "asked": set(),
         "name": name,
